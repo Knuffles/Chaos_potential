1df8fe526b85401abd03ead7e9c45bbead10049c
MeiMeitonymie <83699655+MeiMeitonymie@users.noreply.github.com>
2021-11-12 15:02:02 +0100

    Add files via upload

--- multiproc.py
+++ multiproc.py
@@ -2,28 +2,30 @@ import numpy as np
 import multiprocessing as mp
 import Kepler as kp
 
-def random_init_RK4(E):
+def random_init_RK4(i, E):
     x,y,vx,vy=kp.initialConditions(E)
     w=[x,y,vx,vy]
-    print(w)
     L,T,e = kp.RK4(1e-3,100000,kp.majpos_HH,w,0)
-    print(L)
     return(L)
-
 
 pool=mp.Pool(mp.cpu_count())
 results = []
+E=[0.001,0.002,0.003]
 
 def collect_result(result):
-    global results
-    results.append(result)
+    #global results
+    #results.append(result)
+    return(result)
     
-for i in range(50):
-    pool.apply_async(random_init_RK4, args=(0.01), callback=collect_result)
+for i in range(10):
+    r = pool.apply_async(random_init_RK4, args=(i, 0.01), callback=collect_result)
+    print("iteration "+str(i))
+    results.append(r.get())
+    #print(results)
 
 pool.close()
 pool.join()
 print(results)
-print("bonjour")
+
 
 #print(random_init_RK4(0.01))
