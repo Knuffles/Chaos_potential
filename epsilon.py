1ca0a341d9a3867ea8107afe0f138cefc8554f66
MeiMeitonymie <83699655+MeiMeitonymie@users.noreply.github.com>
2021-12-03 16:14:23 +0100

    Update epsilon.py

--- epsilon.py
+++ epsilon.py
@@ -2,6 +2,7 @@ import Kepler as kp
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy.linalg import norm 
+from sklearn import linear_model
 
 #add a small variation e to the initial condition in x
 def epsilonInit(w,e):
@@ -40,6 +41,14 @@ if __name__ == '__main__':
         Frac.append(abs(norm(L[i,:])-norm(Leps[i,:])))
         t.append(i)
     
+    T=np.array(t).reshape((-1,1))
+    F=np.array(Frac)
+    print(np.shape(T))
+    print(np.shape(F))
+    regr = linear_model.LinearRegression()
+    regr.fit(T,F)
+    droite = regr.coef_
+    
     #tracé des deux trajectoires
 #    plt.plot(L[:,0],L[:,1], label='rk4')
 #    plt.plot(Leps[:,0],Leps[:,1], label='rk4+eps')
@@ -50,13 +59,15 @@ if __name__ == '__main__':
 #    plt.loglog()
     
     #tracé de la différence entre les deux trajectoires dans l'espace des phases
-    plt.plot(t[40:],Frac[40:])
-    plt.loglog()
+
+    plt.xlabel("log(t)")
+    plt.ylabel("log(|D-Deps|)")
     plt.title("E=%f" %E)
-    plt.Xlabel("log(t)")
-    plt.Ylabel("log(|D-Deps|)")
+    plt.loglog(t[40:],Frac[40:])  
+    plt.loglog(t[40:],droite[40:])
+
     #tracé dans l'espace des phases
-    print(len(sectionV),len(sectionY))
+#    print(len(sectionV),len(sectionY))
     
 #    plt.scatter(sectionY,sectionV,marker='+')
 #    plt.scatter(sectionYeps,sectionVeps,marker='+')
