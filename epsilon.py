bd7dc97ca5f07880989e831c1f57c86d013422b7
MeiMeitonymie <83699655+MeiMeitonymie@users.noreply.github.com>
2021-12-03 14:46:51 +0100

    Update epsilon.py

--- epsilon.py
+++ epsilon.py
@@ -1,14 +1,7 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov 26 14:53:20 2021
-
-@author: 33649
-"""
-
-
 import Kepler as kp
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy.linalg import norm 
 
 #add a small variation e to the initial condition in x
 def epsilonInit(w,e):
@@ -18,48 +11,53 @@ def epsilonInit(w,e):
     
 if __name__ == '__main__':
     #initial parameters
-    E = 1/12
-    eps = 0
+    E = 1/6
+    eps = 0.00001
     
     
     #random initial conditions
     w = kp.initialConditions(E)
     #initial condition with small variation
-    eps = 0.1 * w[0]
     weps = epsilonInit(w,eps)
-    print(w[0])
-    print(weps[0])
     L=[]
     Leps=[]
     DL=[]
+    Frac=[]
     t=[]
     sectionV=[]
     sectionY=[]
     sectionVeps=[]
     sectionYeps=[]
     
-    L = kp.RK4(1e-3,2000000,kp.majpos_HH,w,0)
-    Leps = kp.RK4(1e-3,2000000,kp.majpos_HH,weps,0)
-    
-    for i in range(len(L)):
-        DL.append(np.sqrt((L[i,0]-Leps[i,0])**2 + (L[i,1]-Leps[i,1])**2))
-        t.append(i)
+    L = kp.RK4(1e-3,1000000,kp.majpos_HH,w,0)
+    Leps = kp.RK4(1e-3,1000000,kp.majpos_HH,weps,0)
     
     sectionY,sectionV = kp.poincarreSection(L)
     sectionYeps, sectionVeps = kp.poincarreSection(Leps)
+    
+    for i in range(len(L)):
+        DL.append(np.sqrt((L[i,0]-Leps[i,0])**2 + (L[i,1]-Leps[i,1])**2))
+        Frac.append(abs(norm(w)-norm(weps)))
+        t.append(i)
     
     #tracé des deux trajectoires
 #    plt.plot(L[:,0],L[:,1], label='rk4')
 #    plt.plot(Leps[:,0],Leps[:,1], label='rk4+eps')
 #    plt.legend()
     
-    # tracé de la différence entre les deux trajectoires
-    plt.plot(t,DL)
+    #tracé de la différence entre les deux trajectoires
+#    plt.plot(t[40:],DL[40:])
+#    plt.loglog()
     
+    #tracé de la différence entre les deux trajectoires dans l'espace des phases
+    plt.plot(t[40:],Frac[40:])
+    plt.loglog()
     #tracé dans l'espace des phases
     print(len(sectionV),len(sectionY))
     
-    plt.scatter(sectionY,sectionV,marker='+')
-    plt.scatter(sectionYeps,sectionVeps,marker='+')
-    plt.xlabel('position along y')
-    plt.ylabel('speed along y')
+#    plt.scatter(sectionY,sectionV,marker='+')
+#    plt.scatter(sectionYeps,sectionVeps,marker='+')
+#    plt.xlabel('position along y')
+#    plt.ylabel('speed along y')
+#    
+
